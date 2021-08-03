0828543e2865f584bbd91ec9527ed92ce92fbdc5
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2021-08-03 22:18:46 +0300

    test accuracy is bad, need tuning for imagenet-tiny-200
    TODO: try to train cifar100 model and attach it to tutorial

--- examples/torch/classification/quantization.py
+++ examples/torch/classification/quantization.py
@@ -1,6 +1,5 @@
 import json
 import os
-import shutil
 import time
 from urllib.request import urlretrieve
 
@@ -24,93 +23,47 @@ def download_tinyImg200(path,
                         tarname='tiny-imagenet-200.zip'):
     if not os.path.exists(path):
         os.mkdir(path)
-    urlretrieve(url, os.path.join(path, tarname))
-    print(os.path.join(path, tarname))
+    archive_path = os.path.join(path, tarname)
+    urlretrieve(url, archive_path)
+    print(archive_path)
     import zipfile
-    zip_ref = zipfile.ZipFile(os.path.join(path, tarname), 'r')
+    zip_ref = zipfile.ZipFile(archive_path, 'r')
     zip_ref.extractall()
     zip_ref.close()
 
 
-DATASET_DIR = '/media/ssd'
-WORKING_DIR = '/home/nlyalyus/Developer/sandbox/tmp/int8_tutorial'
+DATASET_DIR = 'tiny-imagenet-200'
+DOWNLOAD_DIR = '/media/ssd'
 
-if not os.path.exists(os.path.join(DATASET_DIR, 'tiny-imagenet-200')):
-    download_tinyImg200('/media/ssd')
+if not os.path.exists(DATASET_DIR):
+    download_tinyImg200(DOWNLOAD_DIR)
 
 
-def main(params):
-    arch = params['arch']
-    num_classes = params['num_classes']
-    init_lr = params['init_lr']
-    # momentum = params['momentum']
-    # weight_decay = params['weight_decay']
-    batch_size = params['batch_size']
-    workers = params['workers']
-    pretrained = params['pretrained']
-    resume = params['resume']
-    checkpoint_compressed = params['checkpoint_compressed']
-    data = params['data']
-    evaluate = params['evaluate']
-    start_epoch = params['start_epoch']
-    epochs = params['epochs']
-
-    use_nncf = params['use_nncf']
-    nncf_config_file = params['nncf_config_file']
-
-    best_acc1 = 0
+def main():
+    num_classes = 200  # 200 is for tiny-imagenet, default is 1000 for imagenet
+    init_lr = 1e-4
+    batch_size = 256
+    workers = 4
+    data = DATASET_DIR  # path to data
+    start_epoch = 0
+    epochs = 1  # 15 is for full precision training, will be updated (increased) in case of tuning with nncf
 
     # create model
-    if pretrained:
-        print("=> using pre-trained model '{}'".format(arch))
-        model = models.__dict__[arch](pretrained=True)
-    else:
-        print("=> creating model '{}'".format(arch))
-        model = models.__dict__[arch]()
+    model = models.resnet18(pretrained=True)
     # update the last FC layer for tiny-imagenet number of classes
-    model.fc = nn.Linear(in_features=512, out_features=num_classes, bias=True)
-
-    if not torch.cuda.is_available():
-        print('using CPU, this will be slow')
-    else:
-        print('using GPU')
-        model.cuda()
-        model.fc = model.fc.cuda()
-
-    # define loss function (criterion) and optimizer
-    criterion = nn.CrossEntropyLoss().cuda()
-
-    # TODO: add the parameter in config files for optimizer type
-    # optimizer = torch.optim.SGD(model.parameters(), init_lr,
-    #                             momentum=momentum,
-    #                             weight_decay=weight_decay)
-    optimizer = torch.optim.Adam(model.parameters(), lr=init_lr)
-
-    # optionally resume from a checkpoint
-    if resume:
-        if os.path.isfile(resume):
-            print("=> loading checkpoint '{}'".format(resume))
-            checkpoint = torch.load(resume)
-
-            start_epoch = checkpoint['epoch']
-            print('resumed start_epoch', start_epoch)
-            best_acc1 = checkpoint['best_acc1']
-
-            model.load_state_dict(checkpoint['state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            print("=> loaded checkpoint '{}' (epoch {}, best_acc1 {:6.2f})"
-                  .format(resume, checkpoint['epoch'], checkpoint['best_acc1']))
-        else:
-            print("=> no checkpoint found at '{}'".format(resume))
+    model.fc.num_features = num_classes
+    # self.fc = nn.Linear(512 * block.expansion, num_classes)
+    # model.fc = nn.Linear(in_features=512, out_features=num_classes, bias=True)
+    model.cuda()
+    # model.fc = model.fc.cuda()
 
     # Data loading code
-    traindir = os.path.join(data, 'train')
-    valdir = os.path.join(data, 'val')
+    train_dir = os.path.join(data, 'train')
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
     dataset = datasets.ImageFolder(
-        traindir,
+        train_dir,
         transforms.Compose([
             transforms.Resize(224),
             transforms.RandomHorizontalFlip(),
@@ -127,49 +80,38 @@ def main(params):
         val_dataset, batch_size=batch_size, shuffle=False,
         num_workers=workers, pin_memory=True)
 
-    if use_nncf:
-        # Load a configuration file to specify compression
-        nncf_config = NNCFConfig.from_json(nncf_config_file)
-        # Provide data loaders for compression algorithm initialization, if necessary
-        nncf_config = register_default_init_args(nncf_config, train_loader, criterion)
-        # Apply the specified compression algorithms to the model
-        print('=> compressing the model with {}'.format(nncf_config_file))
-        compression_ctrl, model = create_compressed_model(model, nncf_config)
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().cuda()
+    optimizer = torch.optim.Adam(model.parameters(), lr=init_lr)
+    validate(val_loader, model, criterion)
 
-    if evaluate:
-        validate(val_loader, model, criterion)
-        return
+    nncf_config_dict = {
+        "input_info": {
+            "sample_size": [1, 3, 224, 224]
+        },
+        "compression": {
+            "algorithm": "quantization",  # specify the algorithm here
+        }
+    }
+
+    # Load a configuration file to specify compression
+    nncf_config = NNCFConfig.from_dict(nncf_config_dict)
+    # Provide data loaders for compression algorithm initialization, if necessary
+    nncf_config = register_default_init_args(nncf_config, train_loader)
+
+    compression_ctrl, model = create_compressed_model(model, nncf_config)
 
     for epoch in range(start_epoch, epochs):
         adjust_learning_rate(optimizer, epoch, init_lr)
 
-        if use_nncf:
-            # update compression scheduler state at the begin of the epoch
-            compression_ctrl.scheduler.epoch_step()
-            # train for one epoch with nncf
-            train(train_loader, model, criterion, optimizer, epoch, use_nncf, compression_ctrl=compression_ctrl)
-        else:
-            # train for one epoch without nncf
-            train(train_loader, model, criterion, optimizer, epoch, use_nncf, compression_ctrl=None)
+        # train for one epoch with nncf
+        train(train_loader, model, criterion, optimizer, epoch)
 
         # evaluate on validation set
-        acc1 = validate(val_loader, model, criterion)
-
-        # remember best acc@1 and save checkpoint
-        is_best = acc1 > best_acc1
-        best_acc1 = max(acc1, best_acc1)
-
-        if not use_nncf:
-            save_checkpoint({
-                'epoch': epoch + 1,
-                'arch': arch,
-                'state_dict': model.state_dict(),
-                'best_acc1': best_acc1,
-                'optimizer': optimizer.state_dict(),
-            }, is_best)
+        validate(val_loader, model, criterion)
 
 
-def train(train_loader, model, criterion, optimizer, epoch, use_nncf, compression_ctrl):
+def train(train_loader, model, criterion, optimizer, epoch):
     batch_time = AverageMeter('Time', ':6.3f')
     data_time = AverageMeter('Data', ':6.3f')
     losses = AverageMeter('Loss', ':.4e')
@@ -188,9 +130,6 @@ def train(train_loader, model, criterion, optimizer, epoch, use_nncf, compressio
         # measure data loading time
         data_time.update(time.time() - end)
 
-        if use_nncf:
-            compression_ctrl.scheduler.step()
-
         if torch.cuda.is_available():
             images = images.cuda()
             target = target.cuda()
@@ -198,10 +137,6 @@ def train(train_loader, model, criterion, optimizer, epoch, use_nncf, compressio
         # compute output
         output = model(images)
         loss = criterion(output, target)
-
-        if use_nncf:
-            compression_loss = compression_ctrl.loss()
-            loss += compression_loss
 
         # measure accuracy and record loss
         acc1, acc5 = accuracy(output, target, topk=(1, 5))
@@ -261,17 +196,10 @@ def validate(val_loader, model, criterion):
             if i % print_frequency == 0:
                 progress.display(i)
 
-        # TODO: this should also be done with the ProgressMeter
         print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
               .format(top1=top1, top5=top5))
 
     return top1.avg
-
-
-def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
-    torch.save(state, os.path.join(WORKING_DIR, filename))
-    if is_best:
-        shutil.copyfile(os.path.join(WORKING_DIR, filename), os.path.join(WORKING_DIR, 'model_best.pth.tar'))
 
 
 class AverageMeter(object):
@@ -384,42 +312,4 @@ def create_json_files(batch_size, input_size):
     return configs
 
 
-params = {}
-
-params['arch'] = 'resnet18'
-params['num_classes'] = 200  # 200 is for tiny-imagenet, default is 1000 for imagenet
-params['data'] = 'tiny-imagenet-200'  # path to data
-
-params['init_lr'] = 1e-4
-params['batch_size'] = 256
-params['workers'] = 4
-params['start_epoch'] = 0  # updated automatically if training is resumed
-params['epochs'] = 15  # 15 is for full precision training, will be updated (increased) in case of tuning with nncf
-params['pretrained'] = True  # pretrained model on Imagenet
-
-params['resume'] = WORKING_DIR + 'model_best.pth.tar'  # path to latest checkpoint (or None)
-params['checkpoint_compressed'] = False
-
-params['evaluate'] = False
-
-params['use_nncf'] = True
-
-if params['use_nncf']:
-    # create all config files
-    configs = create_json_files(params['batch_size'], input_size=224)
-
-    # choose config file
-    algorithm_config = 'quantization.json'
-    params['nncf_config_file'] = 'config_files/' + algorithm_config
-
-    # update tune params to fit certain compression algorithm
-    params['init_lr'] = configs[algorithm_config]['optimizer']['base_lr']
-    params['epochs'] = params['epochs'] + configs[algorithm_config]['epochs']
-
-# Iterate over algorithms
-algorithm_config = 'quantization.json'
-# Run the tuning procedure:
-print(algorithm_config)
-params['nncf_config_file'] = 'config_files/' + algorithm_config
-# update tune params
-main(params)
+main()
