8c784382ec5d933be6b2a64e0151578b2df3d2ee
Vasily Shamporov <vasily.shamporov@intel.com>
2022-10-14 15:45:02 +0200

    Use torch 1.12

--- setup.py
+++ setup.py
@@ -118,14 +118,14 @@ EXTRAS_REQUIRE = {
         "tensorflow~=2.8.2",
     ],
     "torch": [
-        "torch>=1.5.0, <=1.9.1, !=1.8.0",
+        "torch~=1.12",
         # Please see
         # https://stackoverflow.com/questions/70520120/attributeerror-module-setuptools-distutils-has-no-attribute-version
         "setuptools==59.5.0"
     ],
     "onnx": [
-        "torch==1.9.1",
-        "torchvision==0.10.1",
+        "torch~=1.12",
+        "torchvision~=0.13",
         "onnx==1.11.0",
         "skl2onnx==1.9.3",
         "protobuf==3.20.1",
