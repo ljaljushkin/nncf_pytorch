2d3c3505f5a40df6ff4aeacf1d03dc88f17d42e8
Nikolay Lyalyushkin <nikolay.lyalyushkin@intel.com>
2022-11-04 13:56:33 +0100

    Adjusted torch.export for torch 1.12

--- nncf/torch/exporter.py
+++ nncf/torch/exporter.py
@@ -157,13 +157,15 @@ class PTExporter(Exporter):
                 retval = dummy_forward(self._model)
                 output_names = generate_output_names_list(count_tensors(retval))
 
-            torch.onnx.export(model, tuple(input_tensor_list), save_path,
+            torch.onnx.export(model=model,
+                              args=tuple(input_tensor_list),
+                              f=save_path,
                               input_names=input_names,
                               output_names=output_names,
-                              enable_onnx_checker=False,
+                              # enable_onnx_checker=False,
                               opset_version=opset_version,
                               # Do not fuse Conv+BN in ONNX. May cause dropout elements to appear in ONNX.
-                              training=True)
+                              training=torch.onnx.TrainingMode.TRAINING)
             model.enable_dynamic_graph_building()
         model.forward = original_forward
         model.to(original_device)
