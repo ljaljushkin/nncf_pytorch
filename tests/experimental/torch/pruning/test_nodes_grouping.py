4c27d253291df3d5bcf2e621b66350e30f2163a8
Nikolay Lyalyushkin <nikolay.lyalyushkin@intel.com>
2023-02-07 15:02:04 +0100

    Fixed issue with BERT because GELU is not patched in simple nncf import
    need to do "import nncf.torch"

--- tests/experimental/torch/pruning/test_nodes_grouping.py
+++ tests/experimental/torch/pruning/test_nodes_grouping.py
@@ -5,12 +5,27 @@ from typing import List
 import numpy as np
 import pytest
 import torch
+# TODO: why "import nncf" is not enough for calling patch_torch_operators??
+import nncf.torch
+
 from torch import nn
 from transformers import AutoModelForQuestionAnswering
 from transformers import AutoModelForSequenceClassification
 from transformers import BertConfig
 
 from nncf import NNCFConfig
+from transformers import CLIPVisionModel
+# TODO: why this import breaks BERT tests???
+#             assert len(output_tensors_shapes) == 1 or len(set(output_tensors_shapes)) <= 1, node.node_name
+#  >           output_tensors_shape = output_tensors_shapes[0]
+#  E           IndexError: list index out of range
+#  need to dump graph, seems like some operation is not wrapped because of order of imports?
+#  ANSWER: gelu is not patched, graph is disjointed and linear has no output shapes, which is not expected.
+
+from transformers import CLIPVisionConfig
+from transformers import GPT2Config
+from transformers import MobileBertConfig
+
 from nncf.experimental.common.pruning.nodes_grouping import MinimalDimensionBlock
 from nncf.experimental.common.pruning.nodes_grouping import PruningNodeGroup
 from nncf.experimental.common.pruning.nodes_grouping import get_pruning_groups
@@ -135,6 +150,100 @@ TEST_DESCS = [
             )
         ]
     ),
+    GroupTestDesc(
+        model_desc=GeneralModelDesc(
+            model_name='MobileBERT',
+            input_info=[dict(sample_size=[1, 128], type='long')] * 4,
+            model_builder=partial(AutoModelForSequenceClassification.from_config,
+                                  MobileBertConfig(
+                                      hidden_size=4,
+                                      intermediate_size=3,
+                                      max_position_embeddings=128,
+                                      num_attention_heads=2,
+                                      num_hidden_layers=1,
+                                      vocab_size=10,
+                                      num_labels=2,
+                                      mhsa_qkv_bias=True,
+                                      mhsa_o_bias=True,
+                                      ffn_bias=True
+                                  ))
+        ),
+        ref_groups=[
+            # Why was this group filtered out?
+            # PruningNodeGroup(
+            #     dim_blocks={
+            #         MinimalDimensionBlock(size=1, offset=0, producer_id=22, pruning_dimension=0),
+            #         MinimalDimensionBlock(size=1, offset=0, producer_id=24, pruning_dimension=1),
+            #         MinimalDimensionBlock(size=1, offset=0, producer_id=25, pruning_dimension=1),
+            #     }
+            # ),
+            PruningNodeGroup(
+                dim_blocks={
+                    MinimalDimensionBlock(size=64, offset=0, producer_id=26, pruning_dimension=0),
+                    MinimalDimensionBlock(size=64, offset=0, producer_id=27, pruning_dimension=0),
+                    MinimalDimensionBlock(size=64, offset=0, producer_id=25, pruning_dimension=0),
+                    MinimalDimensionBlock(size=64, offset=0, producer_id=44, pruning_dimension=1)
+                }
+            ),
+            PruningNodeGroup(
+                dim_blocks={
+                    MinimalDimensionBlock(size=1, offset=0, producer_id=50, pruning_dimension=1),
+                    MinimalDimensionBlock(size=1, offset=0, producer_id=48, pruning_dimension=0)
+                }
+            ),
+            PruningNodeGroup(
+                dim_blocks={
+                    MinimalDimensionBlock(size=1, offset=0, producer_id=56, pruning_dimension=1),
+                    MinimalDimensionBlock(size=1, offset=0, producer_id=54, pruning_dimension=0)
+                }
+            ),
+            PruningNodeGroup(
+                dim_blocks={
+                    MinimalDimensionBlock(size=1, offset=0, producer_id=62, pruning_dimension=1),
+                    MinimalDimensionBlock(size=1, offset=0, producer_id=60, pruning_dimension=0)
+                }
+            ),
+            PruningNodeGroup(
+                dim_blocks={
+                    MinimalDimensionBlock(size=1, offset=0, producer_id=68, pruning_dimension=1),
+                    MinimalDimensionBlock(size=1, offset=0, producer_id=66, pruning_dimension=0)
+                }
+            ),
+            PruningNodeGroup(
+                dim_blocks={
+                    MinimalDimensionBlock(size=1, offset=0, producer_id=81, pruning_dimension=1),
+                    MinimalDimensionBlock(size=1, offset=0, producer_id=78, pruning_dimension=0)
+                }
+            )
+        ]
+    ),
+    GroupTestDesc(
+        model_desc=GeneralModelDesc(
+            model_name='GPT2Text',
+            input_info=[dict(sample_size=[1, 128], type='long')] * 1,
+            model_builder=partial(AutoModelForSequenceClassification.from_config,
+                                  GPT2Config(n_embd=4, n_layer=2, n_head=2, vocab_size=2))
+        ),
+        ref_groups=[]
+    ),
+    GroupTestDesc(
+        model_desc=GeneralModelDesc(
+            model_name='CLIP',
+            input_info=[dict(sample_size=[1, 3, 3, 3], type='float')] * 1,
+            model_builder=partial(CLIPVisionModel,
+                                  CLIPVisionConfig(
+                                      hidden_size=2,
+                                      intermediate_size=2,
+                                      num_hidden_layers=1,
+                                      num_attention_heads=2,
+                                      num_channels=3,
+                                      image_size=3,
+                                      patch_size=3,
+                                      # n_embd=4, n_layer=2, n_head=2, vocab_size=2
+                                  ))
+        ),
+        ref_groups=[]
+    ),
     # TODO: add Swin and Wave2Vec
 ]
 
@@ -147,7 +256,7 @@ def test_graph(desc: GroupTestDesc):
     model = model_desc.get_model()
     config = NNCFConfig({"input_info": model_desc.create_input_info()})
     nncf_network = create_nncf_network(model, config)
-
+    nncf_network.get_graph().visualize_graph('nncf_network.dot')
     pruning_producing_types = [x.op_func_name for x in NNCF_PRUNING_MODULES_DICT]
     actual_groups = get_pruning_groups(nncf_network.get_graph(),
                                        PT_EXPERIMENTAL_PRUNING_OPERATOR_METATYPES,
