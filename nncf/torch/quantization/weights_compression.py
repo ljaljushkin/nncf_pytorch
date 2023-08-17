61dacae45e9696baf56d580061b7f455a95a0621
Nikolay <nikolay.lyalyushkin@intel.com>
2023-08-17 14:47:46 +0200

    more experiments with mixed precision

--- nncf/torch/quantization/weights_compression.py
+++ nncf/torch/quantization/weights_compression.py
@@ -13,6 +13,7 @@ from typing import Dict, Optional
 
 import torch
 from torch import nn
+import numpy as np
 
 from nncf.torch.layers import NNCF_WRAPPED_USER_MODULES_DICT
 from nncf.torch.layers import NNCFEmbedding
@@ -60,9 +61,46 @@ class WeightsFQ(nn.Module):
             layer.weight, self.scale, self.zero_point, self.axis, 0, self.level_high
         )
 
+def get_rnd_err(layer):
+    if not type(layer) is NNCFLinear:
+        return -1.0
+
+    target_dim = layer.target_weight_dim_for_compression
+    stat_dim = (target_dim + 1) % 2
+    input_low = torch.min(layer.weight, dim=stat_dim)[0].detach()
+    input_high = torch.max(layer.weight, dim=stat_dim)[0].detach()
+
+    level_high = 2**4-1
+
+    scale, zero_point = get_scale_zp_from_input_low_input_high(0, level_high, input_low, input_high)
+
+    scale = scale.unsqueeze(stat_dim)
+    zero_point = zero_point.unsqueeze(stat_dim)
+
+    compressed_weight = layer.weight.data / scale + zero_point
+    compressed_weight = torch.clamp(torch.round(compressed_weight), 0, level_high)
+
+    w = compressed_weight.type(dtype=scale.dtype)
+    w = (w - zero_point) * scale
+
+    diff = (w - layer.weight.data)**2
+    mean_err = torch.mean(diff)
+    layer_err = torch.mean(diff, dim=1)
+    top_k = torch.topk(layer_err, 10)[0]
+    #val = float(mean_err)#top_k[0])
+    val = float(top_k[0])
+    return val
+
+def get_rnd_errors(module, res):
+    for name, layer in module.named_children():
+        if not type(layer) is NNCFLinear:
+            get_rnd_errors(layer, res)
+            continue
+
+        res.append(get_rnd_err(layer))
 
 def _insert_pre_compression_operations(
-    module: nn.Module, allowed_types: Dict, use_fake_quantize=False, level_high=255
+    module: nn.Module, allowed_types: Dict, use_fake_quantize=False, level_high=255, stats=None
 ) -> Optional[nn.Module]:
     """
     Inserts weights compression with dequantization or quantization pre operation for Linear and Embedding layers.
@@ -75,15 +113,33 @@ def _insert_pre_compression_operations(
     :param level_high: highest  possible value of compressed weights (lower is 0 in assymetric quantization).
     :return: The module with inserted operations. The module is not trainable if use_fake_quantize is False.
     """
-    for _, layer in module.named_children():
+    for name, layer in module.named_children():
         if not type(layer) in allowed_types:
-            _insert_pre_compression_operations(layer, allowed_types, use_fake_quantize, level_high)
+            _insert_pre_compression_operations(layer, allowed_types, use_fake_quantize, level_high, stats)
             continue
+        # if 'embed_in' == name or 'embed_out' == name:
+        # if 'embed_out' == name:
+        #      print(f'name={name} in FP32')
+        #      num_bits = 32
+        #      num_weights = np.prod(layer.weight.shape)
+        #      stats.append((name, num_weights, num_bits))
+        #      continue
+        local_level_high = level_high
+        if name in ['embed_in', 'embed_out', 'query_key_value', 'dense_4h_to_h']:
+            local_level_high = 255
+
+        if len(stats) > 96:
+            local_level_high = 15
+        num_bits = 4 if local_level_high == 15 else 8
+        num_weights = np.prod(layer.weight.shape)
+        stats.append((name, num_weights, num_bits))
+
+        print(f'name={name} level_high={local_level_high} num_bits={num_bits} num_weights={num_weights}')
         target_dim = layer.target_weight_dim_for_compression
         stat_dim = (target_dim + 1) % 2
         input_low = torch.min(layer.weight, dim=stat_dim)[0].detach()
         input_high = torch.max(layer.weight, dim=stat_dim)[0].detach()
-        scale, zero_point = get_scale_zp_from_input_low_input_high(0, level_high, input_low, input_high)
+        scale, zero_point = get_scale_zp_from_input_low_input_high(0, local_level_high, input_low, input_high)
 
         if not use_fake_quantize:
             scale = scale.unsqueeze(stat_dim)
@@ -91,7 +147,7 @@ def _insert_pre_compression_operations(
             layer.register_pre_forward_operation(WeightsDecompressor(zero_point, scale))
 
             compressed_weight = layer.weight.data / scale + zero_point
-            compressed_weight = torch.clamp(torch.round(compressed_weight), 0, level_high)
+            compressed_weight = torch.clamp(torch.round(compressed_weight), 0, local_level_high)
 
             layer.weight.requires_grad = False
             layer.weight.data = compressed_weight.type(dtype=torch.uint8)
@@ -100,7 +156,7 @@ def _insert_pre_compression_operations(
             layer.register_pre_forward_operation(WeightsFQ(zero_point, scale, target_dim))
 
 
-def insert_pre_compression_operations(module: nn.Module, use_fake_quantize=False, bits=8) -> Optional[nn.Module]:
+def insert_pre_compression_operations(module: nn.Module, use_fake_quantize=False, bits=4) -> Optional[nn.Module]:
     """
     Inserts weights compression with dequantization or quantization pre operation for Linear and Embedding layers.
 
@@ -121,4 +177,16 @@ def insert_pre_compression_operations(module: nn.Module, use_fake_quantize=False
     for user_type in user_types:
         allowed_types.append(user_type)
 
-    _insert_pre_compression_operations(module, allowed_types, use_fake_quantize, level_high)
+    stats = []
+    _insert_pre_compression_operations(module, allowed_types, use_fake_quantize, level_high, stats)
+    print(*stats, sep='\n')
+    total_num_weights = sum(num_weights for _, num_weights, _ in stats)
+    compressed_bits = sum(num_weights * num_bits for _, num_weights, num_bits in stats)
+    c = {4: 0, 8:0, 32:0}
+    print(len(stats))
+    for _, num_weights, num_bits in stats:
+        c[num_bits] += num_weights
+    for num_bits, num_weights in c.items():
+        print(f'% weights in {num_bits} = {num_weights / total_num_weights * 100:.3f}')
+    print(f'weight compression={total_num_weights * 32 / compressed_bits :.3f} (fp32 - 1x, int8 - 4x, int4 - 8x, 50% int4/int8 ~ 6x)')
+
