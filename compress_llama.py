a4a83655b644ebacbb8ba142770d2e50eb695a8f
Nikolay <nikolay.lyalyushkin@intel.com>
2023-08-24 11:00:39 +0200

    refactored compress power quant

--- compress_llama.py
+++ compress_llama.py
@@ -1,25 +1,21 @@
 
-from pathlib import Path
-
 import openvino.runtime as ov
 from openvino import Core
-from openvino.runtime import save_model
 
 from nncf import compress_weights
 
-# from optimum.intel import OVModelForCausalLM
+
 core = Core()
 
-MODEL_DIR = Path('/home/nlyaly/projects/nncf/tests/openvino')
+# from optimum.intel import OVModelForCausalLM
 # MODEL_NAME = 'opt-125m'
-MODEL_NAME = 'llama2'
-
 # use_pkv = True
 # ov_model = OVModelForCausalLM.from_pretrained('meta-llama/Llama-2-7b-chat-hf', use_cache=use_pkv, trust_remote_code=True, from_transformers=True)
 # ov_model.save_pretrained('/home/nlyaly/projects/nncf/tests/openvino')
 # ie = ov.Core()
 
-model = core.read_model(model= str(MODEL_DIR / (MODEL_NAME + '.xml')))
-
+SRC_PATH = '/home/devuser/nlyalyus/projects/lm-evaluation-harness/cache/Llama-2-7b-chat-hf/fp32/openvino_model.xml'
+DST_PATH = '/home/devuser/nlyalyus/projects/lm-evaluation-harness/cache/Llama-2-7b-chat-hf/power_quant_ov/openvino_model.xml'
+model = core.read_model(model=SRC_PATH)
 model = compress_weights(model)
-ov.save_model(model, str(MODEL_DIR / (MODEL_NAME + '_pwr.xml')), compress_to_fp16=False)
+ov.save_model(model, DST_PATH, compress_to_fp16=False)
