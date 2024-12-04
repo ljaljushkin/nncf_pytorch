cd1f10c7a94274b2f8808da45e48c0d5b9edc28d
kprokofi <kirill.prokofiev@intel.com>
2024-12-05 01:41:50 +0900

    small fixes, more models

--- add_FQ_with_LoRA.py
+++ add_FQ_with_LoRA.py
@@ -86,10 +86,19 @@ ROOT_MODEL_DIR = Path.home() / ("MODEL_DIR")
 # model_id = "TinyLlama/TinyLlama_v1.1"
 # model_id = "microsoft/Phi-3-mini-4k-instruct"
 # model_id = "microsoft/Phi-3.5-mini-instruct"
-model_id = "HuggingFaceTB/SmolLM-1.7B-Instruct"
+# model_id = "HuggingFaceTB/SmolLM-1.7B-Instruct"
+# model_id = "Qwen/Qwen2.5-3B-Instruct"
+# model_id = 'google/gemma-2-2b-it'
+# model_id = 'meta-llama/Meta-Llama-3-8B'
+# model_id = 'mistralai/Mistral-7B-v0.3'
+model_id = 'meta-llama/Llama-3.2-1B-Instruct'
+# model_id = 'meta-llama/Llama-3.2-3B-Instruct'
+
 model_name = Path(model_id).name.replace(".", "_")
 
 MODEL_DIR = ROOT_MODEL_DIR / model_name
+MODEL_DIR.mkdir(exist_ok=True, parents=True)
+assert MODEL_DIR.exists()
 
 hf_model = AutoModelForCausalLM.from_pretrained(
     model_id,
@@ -141,14 +150,15 @@ nncf.compress_weights(
 )
 
 
-layer = model._nncf.external_quantizers.FQ_LORA_for_node_layers_23_mlp_down_proj_weight
-print("23dj_IL=", torch.linalg.norm(layer.input_low.data).item())
-print("23dj_IR=", torch.linalg.norm(layer.input_range.data).item())
+# layer = model._nncf.external_quantizers.FQ_LORA_for_node_layers_23_mlp_down_proj_weight
+# print("23dj_IL=", torch.linalg.norm(layer.input_low.data).item())
+# print("23dj_IR=", torch.linalg.norm(layer.input_range.data).item())
 
 # generate_overfit(hf_model, tokenizer, "Quantized")
 # TODO: next experiment with the best params
 ckpt_dir = MODEL_DIR / "FQ_4bit_no_embed_svd_rank256_g64_hybrid_rand_quant100+_sqrtS"
 # ckpt_dir = MODEL_DIR / "FQ_4bit_no_embed_svd_rank8"
+ckpt_dir.mkdir(exist_ok=True, parents=True)
 save_checkpoint(hf_model.model, ckpt_dir)
 model.nncf.get_graph().visualize_graph(ckpt_dir / "fq_model.dot")
 exit()
