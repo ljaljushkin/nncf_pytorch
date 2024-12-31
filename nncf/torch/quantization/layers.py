7696cceda6743c25adbbc2b306d40a7af47929bb
kprokofi <kirill.prokofiev@intel.com>
2025-01-01 01:06:12 +0900

    minor comment

--- nncf/torch/quantization/layers.py
+++ nncf/torch/quantization/layers.py
@@ -1017,7 +1017,7 @@ class FQLoRA_sym(torch.autograd.Function):
         signed_scale = True
         ll_lh = level_low / level_high
         if signed_scale and level_low != 0:
-            input_low = torch.where(scale > 0, -scale, -scale/ll_lh) # s>0 [-s,-7/8s], s<0 [7/8s,-s]
+            input_low = torch.where(scale > 0, -scale, -scale/ll_lh) # s>0 [-s,7/8s], s<0 [7/8s,-s]
             input_range = torch.abs((2 + 1 / level_low) * scale) # 15/8s  or (2-1/8)s
             # input_low = torch.where(scale < 0, scale, scale * ll_lh)
             # input_range = torch.where(scale < 0, scale * ll_lh - input_low, scale - input_low)
